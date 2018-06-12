16536d2fabe9d7095b216c6f8f088d479fcf8a7c
sayantanm19 <sayantanmaiti@gmail.com>
2018-06-12 13:46:03 +0530

    Optimized the call for each upload and download to reduce processing time

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # traispeedanalyzer
-
+  
 SpeedAnalyzer is a script written in python that can analyze data speeds sourced from the TRAI MySpeed Portal and plot/compare bargraphs.
 
-###Basic Usage:
+### Basic Usage:
 
 Download the speed dataset published by TRAI and edit the script to include the csv file.
--- process_data.py
+++ process_data.py
@@ -2,35 +2,64 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-filename = 'may18_publish.csv'
+filename = 'myspeed.csv'
 df = pd.read_csv(filename)
+print(list(df.columns.values))
 
+#Important Col. Names and Techniques
 #print(df.columns.values.tolist())
-#print(df.loc[df['Service Provider'] == 'VODAFONE'])
 #print(df["Data Speed(Mbps)"].mean())
 #print(df['Service Provider'].unique())
 #print(df['Technology'].unique())
 #print('TOTAL STATES: ' + str(len(states)))
 #print('TOTAL PROVIDERS: ' + str(len(providers)))
 
-speedsdown = []
-speedsup = []
-states = df['LSA'].unique()
+def constant_operator(servicep, technology):
 
-def process(operator):
+    states = df['LSA'].unique()
+    speedsdown =[]
+    speedsup = []
+    
     for state in states:
-        avg_speed_down = (df[(df['LSA'] == state) & (df['Service Provider'] == operator) & (df['Test_type'] == 'Download')]['Data Speed(Mbps)'].mean())
-        if (pd.isnull(avg_speed_down)):
-            avg_speed_down = 0
-        speedsdown.append(avg_speed_down)
-        #upload
-        avg_speed_up = (df[(df['LSA'] == state) & (df['Service Provider'] == operator) & (df['Test_type'] == 'Upload')]['Data Speed(Mbps)'].mean())
-        if (pd.isnull(avg_speed_up)):
-            avg_speed_up = 0
-        speedsup.append(avg_speed_up)
-    plot_double_bar(operator)
+        ttype = (df[(df['LSA'] == state) & (df['Service Provider'] == servicep) &(df['Technology'] == technology)])
+        down = (ttype[ttype['Test_type'] == 'Download']["Data Speed(Mbps)"]).mean()
+        up =(ttype[ttype['Test_type'] == 'Upload']["Data Speed(Mbps)"]).mean()
 
-def plot_double_bar(op):
+        if (pd.isnull(down)):
+            down = 0
+        if (pd.isnull(up)):
+            up = 0
+        #print(down)
+        #print(up)
+        speedsdown.append(down)
+        speedsup.append(up)
+        
+    plot_double_bar_state(states, speedsdown, speedsup)
+        
+        
+
+def constant_state(state, technology):
+    operators = df['Service Provider'].unique()
+    speedsdown =[]
+    speedsup = []
+
+    for servicep in operators:
+        ttype = (df[(df['LSA'] == state) & (df['Service Provider'] == servicep) &(df['Technology'] == technology)])
+        down = (ttype[ttype['Test_type'] == 'Download']["Data Speed(Mbps)"]).mean()
+        up =(ttype[ttype['Test_type'] == 'Upload']["Data Speed(Mbps)"]).mean()
+
+        if (pd.isnull(down)):
+            down = 0
+        if (pd.isnull(up)):
+            up = 0
+        #print(down)
+        #print(up)
+        speedsdown.append(down)
+        speedsup.append(up)
+    plot_double_bar_operator(operators, speedsdown, speedsup)
+    
+
+def plot_double_bar_state(states, speedsdown, speedsup):
     fig, ax = plt.subplots()
     index = np.arange(len(states))
     bar_width = 0.35
@@ -48,16 +77,40 @@ def plot_double_bar(op):
      
     plt.xlabel('States')
     plt.ylabel('Average Speeds')
-    plt.title('Avg. Download speed for different operators for' + str(op))
+    plt.title('Avg. Download speed for different operators for Operator')
     plt.xticks(index + bar_width, states, rotation=90)
     plt.legend()
-     
     plt.tight_layout()
     plt.show()
 
+def plot_double_bar_operator(operators, speedsdown, speedsup):
+    fig, ax = plt.subplots()
+    index = np.arange(len(operators))
+    bar_width = 0.35
+    opacity = 0.8
+    
+    rects1 = plt.bar(index, speedsdown, bar_width,
+                     alpha=opacity,
+                     color='b',
+                     label='Download')
+     
+    rects2 = plt.bar(index + bar_width, speedsup, bar_width,
+                     alpha=opacity,
+                     color='g',
+                     label='Upload')
+     
+    plt.xlabel('Service Providers')
+    plt.ylabel('Average Speeds')
+    plt.title('Avg. Download speed for different operators for State')
+    plt.xticks(index + bar_width, operators, rotation=90)
+    plt.legend()
+    plt.tight_layout()
+    plt.show()
 
-process('JIO')
+constant_operator('AIRTEL', '3G')
+#plot_double_bar_state()
 
-
+#constant_state('Delhi', '3G')
+#plot_double_bar_servicep()
 
 
